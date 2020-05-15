fa82ddb5a77fb0611f72e6fbfb761baa05ebe7c3
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-05-15 17:31:43 +0530

    Added script

---
+++ mainScript.R
@@ -0,0 +1,11 @@
+pollutantmean <- function(directory, pollutant, id=1:332){
+  
+  file_names <- list.files(directory,full.names = TRUE)
+  print(file_names)
+  # empty dataframe to conacatenate all the csv files into one
+  dat <- data.frame() 
+  for (i in id){
+    dat <- rbind(dat,read.csv(file_names[i])) #rbind
+  }
+  
+}
