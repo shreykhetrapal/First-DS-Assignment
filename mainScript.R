61d79e1628d4b656e9b2760d48ddaa82538b2e52
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-05-15 17:42:59 +0530

    unnecessary print function

--- mainScript.R
+++ mainScript.R
@@ -1,7 +1,7 @@
 pollutantmean <- function(directory, pollutant, id=1:332){
   
   file_names <- list.files(directory,full.names = TRUE) 
-  print(file_names)
+  #print(file_names)
   
   # empty dataframe to conacatenate all the csv files into one
   dat <- data.frame() 
