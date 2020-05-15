0bee42a000e58d55ed2619c4e5359903fe784c6d
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-05-15 18:52:31 +0530

    PollutantMean function is back

--- mainScript.R
+++ mainScript.R
@@ -1,3 +1,20 @@
+pollutantmean <- function(directory, pollutant, id=1:332){
+  
+  file_names <- list.files(directory,full.names = TRUE) 
+  #print(file_names)
+  
+  # empty dataframe to conacatenate all the csv files into one
+  dat <- data.frame() 
+  
+  #rbind #dat contains all the values of the requested IDs
+  for (i in id){
+    dat <- rbind(dat,read.csv(file_names[i])) 
+  }
+  
+  mean(dat[[pollutant]],na.rm = TRUE)
+  
+}
+
 complete <- function(directory,id){
   file_names2 <- list.files(directory,full.names = TRUE)
   
@@ -25,4 +42,5 @@ complete <- function(directory,id){
   
   colnames(dat3)<-c('id','nobs')
   dat3
+  
 }
