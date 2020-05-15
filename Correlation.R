81fe24637b26fdd7ef1f69299a73a4a98d27a7df
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-05-15 22:51:30 +0530

    Correlation function added in a new file

---
+++ Correlation.R
@@ -0,0 +1,23 @@
+corr <- function(directory,threshold=0){
+  file_names3 <- list.files(directory,full.names = TRUE)
+
+  dat4 <- data.frame()
+  
+  for (i in 1:332){
+    dat4 <- rbind(dat4,read.csv(file_names3[i])) 
+  }
+  
+  completeData2 <- dat4[complete.cases(dat4), ]
+  
+  dat5 <- data.frame() # making dataframe of ID and numberOfCompleteCases
+  
+  for (j in 1:332){
+    totalcases <- sum(completeData2$ID==j)
+    dat5 <- rbind(dat5,c(j,totalcases))
+  }
+  
+  colnames(dat5) <- c('id','total')
+  newDataID <- dat5[dat5$total>threshold, ] #contains IDs of rows with threshold value greater than required
+  
+  
+}
