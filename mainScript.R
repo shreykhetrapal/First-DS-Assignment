e03dc5f6ed64ff0e5206187ea185d76a83c23e46
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-05-15 18:50:51 +0530

    Complete

--- mainScript.R
+++ mainScript.R
@@ -1,35 +1,28 @@
-pollutantmean <- function(directory, pollutant, id=1:332){
-  
-  file_names <- list.files(directory,full.names = TRUE) 
-  #print(file_names)
-  
-  # empty dataframe to conacatenate all the csv files into one
-  dat <- data.frame() 
-  
-  #rbind #dat contains all the values of the requested IDs
-  for (i in id){
-    dat <- rbind(dat,read.csv(file_names[i])) 
-  }
-  
-  mean(dat[[pollutant]],na.rm = TRUE)
-  
-}
-
 complete <- function(directory,id){
   file_names2 <- list.files(directory,full.names = TRUE)
   
   dat2 <- data.frame()
   
   for (i in id){
-  dat2 <- rbind(dat2,read.csv(file_names2[i])) 
+    dat2 <- rbind(dat2,read.csv(file_names2[i])) 
   }
   
   #creating a 3rd dataframe
   
-  dat3 <- matrix(c(0,0),nrow = 1,ncol = 2)
-  dat3 <- data.frame(dat3)
-  colnames(dat3) <- c('id','nobs') #changing the names of the columns
-  dat3 <- dat[-1,] # delete the first extra row with values of 0,0 created by the matrix
+  
+  dat3 <- data.frame() # empty dataframe to store computed values
+  
+  casesComplete <- complete.cases(dat2)
+  
+  completeData <- dat2[casesComplete,] #Note the ',' after [casesComplete, ] it specifies coplete rows all columns
   
   
+  for (j in id){
+    
+    counts <- sum(completeData$ID==j) #
+    dat3 <- rbind(dat3,c(j,counts)) #
+  }
+  
+  colnames(dat3)<-c('id','nobs')
+  dat3
 }
